0e834f98574edcb4c08e466cb55d3fcd852e8d2b
Flancoin <flancointoken@gmail.com>
2024-11-06 00:55:46 +0100

    Initial commit

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,7 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import blockContent from './blockContent';
+import category from './category';
+import post from './post';
+import author from './author';
+import news from './news'; // Importa el esquema de noticias
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, blockContent, news]; // Agrega `news` al array
---
+++ schemaTypes/news.js
@@ -0,0 +1,34 @@
+export default {
+    name: 'news',
+    title: 'News',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+        validation: Rule => Rule.required(),
+      },
+      {
+        name: 'date',
+        title: 'Publication Date',
+        type: 'datetime',
+        validation: Rule => Rule.required(),
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: 'content',
+        title: 'Content',
+        type: 'array',
+        of: [{ type: 'block' }],
+      },
+    ],
+  };
+  
