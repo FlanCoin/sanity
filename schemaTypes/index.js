6fa9280a41421d153f429514f109bd36c91c2084
Flancoin <flancointoken@gmail.com>
2024-11-06 01:47:24 +0100

    Descripción de los cambios realizados

---
+++ schemaTypes/index.js
@@ -0,0 +1,6 @@
+import blockContent from './blockContent';
+import category from './category';
+import post from './post';
+import author from './author';
+import news from './news'; // Importa el esquema de noticias
+export const schemaTypes = [post, author, category, blockContent, news]; // Agrega `news` al array
